4984d81d53adec4d3dcab62e350dcecfb17d4a2f
imoseyon <imoseyon@gmail.com>
2012-03-15 23:34:04 -0700

    X governors: do not hotplug during earlysuspend registration
    to avoid SOD

--- drivers/cpufreq/cpufreq_interactiveX.c
+++ drivers/cpufreq/cpufreq_interactiveX.c
@@ -65,6 +65,7 @@ static struct mutex set_speed_lock;
 // used for suspend code
 static unsigned int enabled = 0;
 static unsigned int suspendfreq = 576000;
+static unsigned int registration = 0;
 
 /* Hi speed to bump to from lo speed when load burst (default max) */
 static u64 hispeed_freq;
@@ -576,7 +577,7 @@ static void interactivex_suspend(int suspend)
 }
 
 static void interactivex_early_suspend(struct early_suspend *handler) {
-     interactivex_suspend(1);
+     if (!registration) interactivex_suspend(1);
 }
 
 static void interactivex_late_resume(struct early_suspend *handler) {
@@ -633,7 +634,9 @@ static int cpufreq_governor_interactivex(struct cpufreq_policy *policy,
 			return rc;
 
 		enabled = 1;
+		registration = 1;
                 register_early_suspend(&interactivex_power_suspend);
+		registration = 0;
                 pr_info("[imoseyon] interactivex start\n");
 		break;
 

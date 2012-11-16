f310a4b3c3d72ee2d3d575d09c681fe70ec2e1ef
Brandon Berhent <bbedward@androiddeveloperalliance.org>
2012-11-16 15:58:12 -0500

    Fix unpack_add_init.sh

--- zip_template/kernel/unpack_add_init.sh
+++ zip_template/kernel/unpack_add_init.sh
@@ -3,12 +3,13 @@ cd /tmp
 mkdir ramdisk
 cd ramdisk
 /tmp/busybox gzip -dc ../boot.img-ramdisk.gz | /tmp/busybox cpio -i
-if [ ! grep -q "*system/etc/init.d*" init.rc ]; then
+if [ -z `/tmp/busybox grep init.d init.rc` ]; then
 	echo '' >> init.rc
 	echo 'service run_parts /system/sbin/bb/busybox run-parts /system/etc/init.d' >> init.rc
-	echo '	  class main' >> init.rc
-	echo '    oneshot' >> init if thei.rc
+	echo '    class main' >> init.rc
+	echo '    oneshot' >> init.rc
 fi
 rm ../boot.img-ramdisk.gz
 /tmp/busybox sh -c "/tmp/busybox find . | /tmp/busybox cpio -o -H newc | /tmp/busybox gzip > ../boot.img-ramdisk.gz"
 cd ..
+
